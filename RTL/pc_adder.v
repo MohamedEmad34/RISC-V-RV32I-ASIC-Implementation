503a6c42f96679292cec20150649f71d8c7efd27
Mohamed_Emad <102392237+MohamedEmad34@users.noreply.github.com>
2026-09-02 19:14:28 +0300

    Create pc_adder.v

---
+++ RTL/pc_adder.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/02/2026 06:09:58 PM
+// Design Name: 
+// Module Name: pc_adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pc_adder (
+    input   [31:0] pc,
+    output  [31:0] pc_plus_4
+);
+
+    assign pc_plus_4 = pc + 32'd4;
+
+endmodule
